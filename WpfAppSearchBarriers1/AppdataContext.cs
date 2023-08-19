ec91a3c3bda35457266fe053cc3c7e3c952aae7e
TrixOMG <nikitin.pavel.163@gmail.com>
2023-08-19 12:03:08 +0400

    initial commit

---
+++ WpfAppSearchBarriers1/AppdataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WpfAppSearchBarriers1;
+
+public partial class AppdataContext : DbContext
+{
+    public AppdataContext()
+    {
+    }
+
+    //public DbSet<Employee> Employees { get; set; }
+    public DbSet<Employee> Table { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB;Initial Catalog = Appdata;");
+}
---
+++ WpfAppSearchBarriers1/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using static iText.Kernel.Pdf.Colorspace.PdfShading;
+using System.Windows.Media;
+
+namespace WpfAppSearchBarriers1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _searchText;
+        private string _sexParam = "male";
+        public string SearchText
+        {
+            get { return _searchText; }
+            set { _searchText = value; }
+        }
+
+        public string SexParam
+        {
+            get { return _sexParam; }
+            set { _sexParam = value; }
+        }
+
+
+        public List<Employee> MyEmployees { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            EmployeesList.ItemsSource = MyEmployees;
+
+            this.DataContext = this;
+        }
+
+        private void Sumbit(object sender, RoutedEventArgs e)
+        {
+            using (AppdataContext _context = new AppdataContext())
+            {
+                string parameter = "%" + SearchText + "%";
+                MyEmployees = _context.Table.Where((e) => EF.Functions.Like(e.Name, parameter)
+                && EF.Functions.Like(e.Sex, SexParam)).ToList();
+                EmployeesList.ItemsSource = MyEmployees;
+            }
+        }
+
+        private void CheckBoxChanged(object sender, RoutedEventArgs e)
+        {
+            _sexParam = "%Female%";
+        }
+        private void Unchecked(object sender, RoutedEventArgs e)
+        {
+            _sexParam = "%male%";
+        }
+
+
+        private void CreatePDF(object sender, RoutedEventArgs e)
+        {
+            //Create datatable
+            DataTable dataTable = ToDataTable(MyEmployees);
+            //Export to PDF
+            ExportDataTableToPdf(dataTable, @"D:\emp.pdf", "Employees Data");
+        }
+
+        private void ExportDataTableToPdf(DataTable dtblTable, String strPdfPath, string strHeader)
+        {
+            string font = @"C:\Windows\Fonts\Arial.ttf";
+
+            System.IO.FileStream fs = new FileStream(strPdfPath, FileMode.Create, FileAccess.Write, FileShare.None);
+            Document document = new Document();
+            document.SetPageSize(iTextSharp.text.PageSize.A4);
+            PdfWriter writer = PdfWriter.GetInstance(document, fs);
+            document.Open();
+
+            //Report Header
+            BaseFont bfntHead = BaseFont.CreateFont(font, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font fntHead = new Font(bfntHead, 16, 1, BaseColor.GRAY);
+            Paragraph prgHeading = new Paragraph();
+            prgHeading.Alignment = Element.ALIGN_CENTER;
+            prgHeading.Add(new Chunk(strHeader.ToUpper(), fntHead));
+            document.Add(prgHeading);
+
+            //Author
+            Paragraph prgAuthor = new Paragraph();
+            BaseFont btnAuthor = BaseFont.CreateFont(font, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+            Font fntAuthor = new Font(btnAuthor, 8, 2, BaseColor.GRAY);
+            prgAuthor.Alignment = Element.ALIGN_RIGHT;
+            prgAuthor.Add(new Chunk("Author : Никитин П. А.", fntAuthor));
+            prgAuthor.Add(new Chunk("\nRun Date : " + DateTime.Now.ToShortDateString(), fntAuthor));
+            document.Add(prgAuthor);
+
+            //Add a line seperation
+            Paragraph p = new Paragraph(new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(0.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_LEFT, 1)));
+            document.Add(p);
+
+            //Add line break
+            document.Add(new Chunk("\n", fntHead));
+
+            //Write the table
+            PdfPTable table = new PdfPTable(dtblTable.Columns.Count);
+            //Table header
+            BaseFont btnColumnHeader = BaseFont.CreateFont(font, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+            Font fntColumnHeader = new Font(btnColumnHeader, 10, 1, BaseColor.WHITE);
+            for (int i = 0; i < dtblTable.Columns.Count; i++)
+            {
+                PdfPCell cell = new PdfPCell();
+                cell.BackgroundColor = BaseColor.GRAY;
+                cell.AddElement(new Chunk(dtblTable.Columns[i].ColumnName.ToUpper(), fntColumnHeader));
+                table.AddCell(cell);
+            }
+            //table Data
+            for (int i = 0; i < dtblTable.Rows.Count; i++)
+            {
+                for (int j = 0; j < dtblTable.Columns.Count; j++)
+                {
+                    table.AddCell(dtblTable.Rows[i][j].ToString());
+                }
+            }
+
+            document.Add(table);
+            document.Close();
+            writer.Close();
+            fs.Close();
+        }
+
+
+
+        //Utility function (converts List to DataTable)
+        public static DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+
+            //Get all the properties
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                //Defining type of data column gives proper data table 
+                var type = (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>) ? Nullable.GetUnderlyingType(prop.PropertyType) : prop.PropertyType);
+                //Setting column names as Property names
+                dataTable.Columns.Add(prop.Name, type);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            //put a breakpoint here and check datatable
+            return dataTable;
+        }
+    }
+}
